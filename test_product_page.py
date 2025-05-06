284c018902d3409901fe94b10cf7bb6b8230544f
TatianaN <wholeriverinfo@gmail.com>
2025-05-06 19:44:44 +0300

    Changed test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,8 @@ import pytest
 from .pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('link_add', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", "offer7", "offer8", "offer9"])
+@pytest.mark.parametrize('link_add', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
+ 
 def test_guest_can_add_product_to_basket(browser, link_add):
     okay_link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={link_add}"
 
@@ -15,4 +16,4 @@ def test_guest_can_add_product_to_basket(browser, link_add):
     product_page.solve_quiz_and_get_code()
     product_page.should_be_add_to_basket()
     
-           
+       
